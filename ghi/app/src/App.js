ee7b47be3f88ee312ba42083b605de23bc4d3d0b
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-10-24 11:55:42 -0700

    Add list for manufacturers.

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,20 +1,49 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
-import ManufacturerForm from './ManufacturerForm';
-import ModelForm from './ModelForm';
-import AutomobileForm from './AutomobileForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
+import Nav from "./Nav";
+import ManufacturerForm from "./ManufacturerForm";
+import ModelForm from "./ModelForm";
+import AutomobileForm from "./AutomobileForm";
+import ManufacturerList from "./ManufacturerList";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [manufacturers, setManufacturers] = useState([]);
+
+  async function getManufacturers() {
+    const url = "http://localhost:8100/api/manufacturers/";
+    const response = await fetch(url);
+    if (response.ok) {
+      const { manufacturers } = await response.json();
+      setManufacturers(manufacturers);
+    } else {
+      console.log("An error occurred fetching the data");
+    }
+  }
+
+  useEffect(() => {
+    getManufacturers();
+  }, []);
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="manufacturer/create/" element={<ManufacturerForm />} />
-          <Route path="models/create/" element={<ModelForm />} />
-          <Route path="automobiles/create/" element={<AutomobileForm />} />
+          <Route path="manufacturer">
+            <Route path="create" element={<ManufacturerForm />} />
+            <Route
+              path="list"
+              element={<ManufacturerList manufacturers={manufacturers} />}
+            />
+          </Route>
+          <Route path="models">
+            <Route path="create" element={<ModelForm />} />
+          </Route>
+          <Route path="automobiles">
+            <Route path="create" element={<AutomobileForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,29 +1,50 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <NavLink className="navbar-brand" to="/">
+          CarCar
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className='nav-item'>
-              <NavLink className="nav-link" to="/manufacturer/create">Create a Manufacturer</NavLink>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/manufacturer/create">
+                Create a Manufacturer
+              </NavLink>
             </li>
-            <li className='nav-item'>
-              <NavLink className="nav-link" to="/models/create">Create a Model</NavLink>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/manufacturer/list">
+                List of Models
+              </NavLink>
             </li>
-            <li className='nav-item'>
-              <NavLink className="nav-link" to="/automobiles/create">Create an Automobile</NavLink>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/models/create">
+                Create a Model
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/automobiles/create">
+                Create an Automobile
+              </NavLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Nav;
