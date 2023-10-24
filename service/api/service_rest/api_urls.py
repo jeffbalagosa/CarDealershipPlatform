b357ea9c9eb553105d4ad0eba4c39cbb211c97e3
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-10-23 20:36:08 -0700

    Remove api_appoint_list method. Add technician_detail.

--- service/api/service_rest/api_urls.py
+++ service/api/service_rest/api_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .api_views import api_list_technicians
+from .api_views import api_list_technicians, technician_detail
 
 urlpatterns = [
     path("technicians/", api_list_technicians, name="api_list_technicians"),
+    path("technicians/<int:pk>/", technician_detail, name="technician_detail"),
 ]
