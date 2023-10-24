bdeb430537b8b891ccf0fc193457f1b541fea42c
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-10-23 21:07:34 -0700

    Add appointments url.

--- service/api/service_rest/api_urls.py
+++ service/api/service_rest/api_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .api_views import api_list_technicians, technician_detail
+from .api_views import api_list_technicians, technician_detail, api_list_appointments
 
 urlpatterns = [
     path("technicians/", api_list_technicians, name="api_list_technicians"),
     path("technicians/<int:pk>/", technician_detail, name="technician_detail"),
+    path("appointments/", api_list_appointments, name="api_list_appointments"),
 ]
