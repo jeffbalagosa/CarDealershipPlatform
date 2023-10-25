3cdce52abcce84aaf25ee8a2d5514d6e39fc4fff
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-10-24 18:43:32 -0700

    Implement filter to only show new appointments. Implement buttons for cancelling and finishing appointments.

--- ghi/app/src/AppointmentList.js
+++ ghi/app/src/AppointmentList.js
@@ -1,4 +1,5 @@
 function AppointmentList(props) {
+  // format date and time using the Date object for more readable display
   const formatDateAndTime = (dateTimeStr) => {
     const dateObj = new Date(dateTimeStr);
     const formattedDate = dateObj.toLocaleDateString("en-US", {
@@ -11,6 +12,41 @@ function AppointmentList(props) {
       minute: "2-digit",
     });
     return { formattedDate, formattedTime };
+  };
+
+  // filter for new appointments so only those are displayed
+  const newAppointments = props.appointments.filter(
+    (appointment) => appointment.status === "created"
+  );
+
+  const handleFinish = async (event, appointmentId) => {
+    event.preventDefault();
+    const finishUrl = `http://localhost:8080/api/appointments/${appointmentId}/finish/`;
+    const fetchOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const hatsResponse = await fetch(finishUrl, fetchOptions);
+    if (hatsResponse.ok) {
+      console.log(`Appointment ${appointmentId} finished!`);
+      window.location.assign("/appointments/list");
+    }
+  };
+
+  const handleCancel = async (event, appointmentId) => {
+    event.preventDefault();
+    const cancelUrl = `http://localhost:8080/api/appointments/${appointmentId}/cancel/`;
+    const fetchOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const hatsResponse = await fetch(cancelUrl, fetchOptions);
+    if (hatsResponse.ok) {
+      console.log(`Appointment ${appointmentId} cancelled!`);
+      window.location.assign("/appointments/list");
+    }
   };
 
   return (
@@ -28,7 +64,7 @@ function AppointmentList(props) {
           </tr>
         </thead>
         <tbody>
-          {props.appointments.map((appointment) => {
+          {newAppointments.map((appointment) => {
             const { formattedDate, formattedTime } = formatDateAndTime(
               appointment.date_time
             );
@@ -41,8 +77,18 @@ function AppointmentList(props) {
                 <td>{appointment.technician}</td>
                 <td>{appointment.reason}</td>
                 <td>
-                  <button className="m-1 btn btn-sm btn-danger">Cancel</button>
-                  <button className="btn btn-sm btn-success">Finish</button>
+                  <button
+                    onClick={(event) => handleCancel(event, appointment.id)}
+                    className="m-1 btn btn-sm btn-danger"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={(event) => handleFinish(event, appointment.id)}
+                    className="btn btn-sm btn-success"
+                  >
+                    Finish
+                  </button>
                 </td>
               </tr>
             );
@@ -56,4 +102,3 @@ function AppointmentList(props) {
 export default AppointmentList;
 
 // TODO: Add VIP Status Column
-// TODO: Add Finish and Cancel Functionality
